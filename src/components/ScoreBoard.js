357e2b5d697010ed051f67cfac81b51080216111
UjwalSai03 <118960538+UjwalSai03@users.noreply.github.com>
2024-01-09 12:11:55 +0530

    Add files via upload

---
+++ src/components/ScoreBoard.js
@@ -0,0 +1,9 @@
+const ScoreBoard = ({ score }) => {
+  return (
+    <div className="score-board">
+      <h2 className="score">Score is : {score}</h2>
+    </div>
+  )
+}
+
+export default ScoreBoard
